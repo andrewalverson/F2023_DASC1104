92045240d0d7f6cc091dfd795ed8cc0e3bc38663
Andrew Alverson <aja@uark.edu>
2023-10-06 16:08:09 -0500

    final script with 3 functions

--- python-scripts/fibonacci.py
+++ python-scripts/fibonacci.py
@@ -6,25 +6,25 @@
 def get_input():
 	number = input("Enter the position in the Fibonacci sequence: ")
 
-	print(number)
-
 	return int(number)
 
 
 # calculate the fib number
-def fib_num():
+def calc_fib(n):
 	# initiate the fib sequence
 	a,b = 0,1
 
-	for i in range(int(number)):
+	for i in range(n):
 		a,b = b,a+b
 
 	fibonacci_number = a
 
+	return fibonacci_number	
+
 
 # print the output
-def print_output():
-	print(fibonacci_number)
+def print_output(user_input, output):
+	print('The Fibonacci number for', user_input, 'is', output)
 
 
 # define main()
@@ -33,13 +33,16 @@ def main():
 	# get the input
 	input_num = get_input()
 	
-	print(input_num, "in main")
-
 	# calcuate the fib number
-#	fib_num()
+	fib_number = calc_fib(input_num)
 
 	# print the output
-#	print_output()
+	print_output(input_num, fib_number)
+
+
+
+
+
 
 
 
